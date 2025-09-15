acf856f6af61bf7215c29d7f2a1493212552a4b4
unknown <ce.rumiakter@gmail.com>
2025-09-15 11:54:53 +0600

    update all

--- src/pages/TrackOrder/TrackOrder.jsx
+++ src/pages/TrackOrder/TrackOrder.jsx
@@ -3,12 +3,12 @@ const TrackOrder = () => {
     return (
         <div className="container mx-auto">
             <div className="bg-white shadow-lg mx-auto w-6/12 border">
-                <h2 className="border-b py-4 text-lg font-medium text-center">Check Your Order Status</h2>
+                <h2 className="border-b py-4 text-lg font-medium text-center dark:text-black">Check Your Order Status</h2>
                 <div className="px-6 my-6">
-                    <input className="border w-full p-2 rounded" type="text" placeholder="Order Code" />
+                    <input className="border w-full dark:bg-white p-2 rounded" type="text" placeholder="Order Code" />
                 </div>
                 <div className="text-center mb-6">
-                    <button className="btn bg-[#FCB840] text-white">Track Order</button>
+                    <button className="btn bg-[#FCB840] text-white dark:border-none">Track Order</button>
                 </div>
             </div>
         </div>
