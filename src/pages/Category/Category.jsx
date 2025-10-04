882562681d4b44fa5811a91d745a4bd73d19195d
unknown <ce.rumiakter@gmail.com>
2025-10-04 17:39:35 +0600

    update

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -5,7 +5,7 @@ const Category = ({ category }) => {
     return (
         <div>
             <div className="bg-white shadow-xl border py-6 rounded-xl">
-                <img className="mx-auto w-28" src={img} alt="" />
+                <img className="mx-auto w-28" src={img} alt="category img" />
                 <h1 className="text-center text-lg font-medium dark:text-black">{title}</h1>
             </div>
         </div>
